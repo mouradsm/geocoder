34d03f8668e6b36f1ee1d609ebc15a8128c8b62c
Diego Moura <diegosouza20049@hotmail.com>
2017-11-28 15:35:25 -0200

    refactor

--- app/Geocode.php
+++ app/Geocode.php
@@ -2,12 +2,14 @@
 
 namespace App;
 
+use Geocoder\Laravel\Facades\Geocoder;
 use Geocoder\Query\GeocodeQuery;
 use \Geocoder\Provider\GoogleMaps\GoogleMaps;
 use \Geocoder\Provider\LocationIQ\LocationIQ;
 use \Geocoder\Provider\PickPoint\PickPoint;
 use \Geocoder\Provider\OpenCage\OpenCage;
 use \Geocoder\ProviderAggregator;
+use \Geocoder\Collection;
 use \Http\Adapter\Guzzle6\Client;
 use App\Address as Address;
 
@@ -26,7 +28,7 @@ class Geocode {
         if ($provider == 'locationiq')
             $this->rateLimit = 10000;
 
-        $enderecos = \App\Address::whereNull('lat')
+        $enderecos = Address::whereNotNull('cep')->whereNull('lat')
                                    ->whereNull('long')->orderBy('id', 'asc')->limit($this->rateLimit)->get();
 
         foreach ($enderecos as $value) {
@@ -34,22 +36,20 @@ class Geocode {
                                                          $value->rua)[0], 'cep' => $value->cep]);
         }
 
-        $correctInformation = [];
-
         foreach ($this->basicInformation as $i) {
             $fullAddress = $i['rua'] . ', ' . $i['cep'] . ', RJ';
 
             $geocode = $this->find($provider, $fullAddress);
 
-            array_push($correctInformation,['id' => $i['id'], 'lat' => $geocode->getLatitude(), 'long' => $geocode->getLongitude()]);
+            if($geocode->count() == 0)
+                continue;
+
+            $geocode = $geocode->get(0)->getCoordinates();
+
+            Address::where('id', '=', $i['id'])->update(['lat' => $geocode->getLongitude(), 'long' => $geocode->getLongitude()]);
 
             sleep(1);
         }
-
-        foreach ($correctInformation as $c) {
-            Address::where('id', '=', $c['id'])->update(['lat' => $c['lat'], 'long' => $c['long']]);
-        }
-
 
     }
 
@@ -63,15 +63,13 @@ class Geocode {
             new OpenCage($this->adapter,env('OPENCAGE_API_KEY'))
         ]);
 
-        $result = $this->geocoder
+        $result =  $this->geocoder
                     ->using($provider)
                     ->geocodeQuery(GeocodeQuery::create($fullAdress));
 
         if($result->count() == 0)
             Log::error('Error on: ' . $fullAdress);
 
-        dd($result->get(0)->getCoordinates());
-
-        return $result->get(0)->getCoordinates();
+        return $result;
     }
 }
