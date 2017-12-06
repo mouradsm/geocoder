0c3421be0a91f63fe97365fd9ef298c95305a928
Diego Moura <diegosouza20049@hotmail.com>
2017-12-05 22:01:28 -0200

    refactoring kernel schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \App\Console\Commands\FindGeocode::class
     ];
 
     /**
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command("geocode:process pickpoint")->dailyAt('6:00');;
+        $schedule->command("geocode:process pickpoint")->dailyAt('22:00');;
         $schedule->command("geocode:process locationiq")->dailyAt('12:00');;
         $schedule->command('geocode:process google_maps')->dailyAt('18:00');;
 
