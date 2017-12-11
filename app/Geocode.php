4e0e693bad393ed8b9f68ef1f30e240e8a37c3bd
Diego Moura <diegosouza20049@hotmail.com>
2017-12-11 21:34:47 -0200

    melhorar para não dar mais crash

--- app/Geocode.php
+++ app/Geocode.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use App\Address as Address;
+use Geocoder\Exception\InvalidServerResponse;
+use Geocoder\Exception\QuotaExceeded;
 use Geocoder\Provider\GoogleMaps\GoogleMaps;
 use Geocoder\Provider\LocationIQ\LocationIQ;
 use Geocoder\Provider\OpenCage\OpenCage;
@@ -10,6 +12,7 @@ use Geocoder\Provider\PickPoint\PickPoint;
 use Geocoder\ProviderAggregator;
 use Geocoder\Query\GeocodeQuery;
 use Http\Adapter\Guzzle6\Client;
+use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Log;
 
 class Geocode {
@@ -24,6 +27,13 @@ class Geocode {
     public function process($provider) {
         if ($provider == 'locationiq')
             $this->rateLimit = 10000;
+
+        $this->geocoder->registerProviders([
+            new GoogleMaps($this->adapter, env('GOOGLE_MAPS_API_KEY')),
+            new LocationIQ($this->adapter, env('LOCATIONIQ_API_KEY')),
+            new PickPoint($this->adapter, env('PICKPOINT_API_KEY')),
+            new OpenCage($this->adapter,env('OPENCAGE_API_KEY'))
+        ]);
 
         $enderecos = Address::whereNotNull('cep')->where('lat', '=', '')
                                    ->where('lng', '=', '')->orderBy('id', 'asc')->limit($this->rateLimit)->get();
@@ -43,30 +53,34 @@ class Geocode {
 
         foreach ($this->basicInformation as $i) {
             $fullAddress = $i['rua'] . ', ' . $i['cep'] . ', RJ';
+            try {
+                $geocode = $this->find($provider, $fullAddress);
 
-            $geocode = $this->find($provider, $fullAddress);
+                if ($geocode->count() == 0)
+                    continue;
 
-            if($geocode->count() == 0)
-                continue;
+                $geocode = $geocode->get(0)->getCoordinates();
 
-            $geocode = $geocode->get(0)->getCoordinates();
+                if (Address::where('id', '=', $i['id'])->update(['lat' => $geocode->getLongitude(), 'lng' => $geocode->getLongitude()]) == 0)
+                    echo $i['id'] . " não atualizado";
 
-            Address::where('id', '=', $i['id'])->update(['lat' => $geocode->getLatitude(), 'lng' => $geocode->getLongitude()]);
+            } catch (QueryException $e) {
+                echo $e->getMessage();
+                Log::error($e->getMessage());
+            } catch (InvalidServerResponse $e ) {
+                echo "Resposta inválida ao procurar: " . $fullAddress;
+                Log::error($e->getMessage());
+            } catch (QuotaExceeded $e) {
+                echo "Limite de cota atingido ao procurar: " . $fullAddress;
+                Log::error($e->getMessage());
+            }
 
             sleep(1);
         }
-
     }
 
     private function find($provider, $fullAdress) {
         echo '.';
-
-        $this->geocoder->registerProviders([
-            new GoogleMaps($this->adapter, env('GOOGLE_MAPS_API_KEY')),
-            new LocationIQ($this->adapter, env('LOCATIONIQ_API_KEY')),
-            new PickPoint($this->adapter, env('PICKPOINT_API_KEY')),
-            new OpenCage($this->adapter,env('OPENCAGE_API_KEY'))
-        ]);
 
         $result =  $this->geocoder
                     ->using($provider)
